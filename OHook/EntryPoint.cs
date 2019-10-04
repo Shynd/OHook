202264508dfe549a1068a4398bba945078836502
Shynd <shynd@protonmail.com>
2019-10-05 01:42:00 +0200

    Build stuff?

---
+++ OHook/EntryPoint.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using EasyHook;
+
+namespace OHook
+{
+    public class EntryPoint : IEntryPoint
+    {
+        #region -- Local Properties --
+
+        private LocalHook CreateFileHook;
+        public static string ChannelName;
+        public readonly ServerInterface Interface;
+        private Queue<string> _messageQueue = new Queue<string>();
+
+        #endregion -- Local Properties --
+
+        #region -- Threads --
+
+
+
+        #endregion -- Threads --
+
+        #region -- Static Properties --
+
+
+
+        #endregion -- Static Properties --
+
+        #region -- Function Pointers --
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
+        delegate IntPtr CreateFile_Delegate(
+            string fileName,
+            UInt32 desiredAccess,
+            UInt32 shareMode,
+            IntPtr securityAttributes,
+            UInt32 creationDisposition,
+            UInt32 flagsAndAttributes,
+            IntPtr templateFile
+        );
+
+        #endregion -- Function Pointers --
+
+        #region -- Hooks --
+
+        IntPtr CreateFile_Hook(
+            string fileName,
+            UInt32 desiredAccess,
+            UInt32 shareMode,
+            IntPtr securityAttributes,
+            UInt32 creationDisposition,
+            UInt32 flagsAndAttributes,
+            IntPtr templateFile)
+        {
+            try
+            {
+                lock (_messageQueue)
+                {
+                    if (_messageQueue.Count < 1000)
+                    {
+                        var mode = string.Empty;
+                        switch (creationDisposition)
+                        {
+                            case 1:
+                                mode = "CREATE_NEW";
+                                break;
+                            case 2:
+                                mode = "CREATE_ALWAYS";
+                                break;
+                            case 3:
+                                mode = "OPEN_ALWAYS";
+                                break;
+                            case 4:
+                                mode = "OPEN_EXISTING";
+                                break;
+                            case 5:
+                                mode = "TRUNCATE_EXISTING";
+                                break;
+                        }
+
+                        // Add message to send to loader
+                        _messageQueue.Enqueue(
+                            string.Format("[{0}:{1}]: CREATE ({2}) \"{3}\"",
+                            RemoteHooking.GetCurrentProcessId(), RemoteHooking.GetCurrentThreadId(),
+                            mode, fileName));
+                    }
+                }
+            }
+            catch
+            {
+                // Swallow exceptions so that any issues caused by this code does not crash the target process.
+            }
+
+            // Modify filename IF the file is in the songs folder
+            if (fileName.Contains(@"\Songs\") && (fileName.Contains(".png") || fileName.Contains(".jpg") || fileName.Contains(".jpeg")))
+            {
+                if (!string.IsNullOrEmpty(Interface.BackgroundImage))
+                {
+                    //_messageQueue.Enqueue("Changed BG for beatmap!");
+                    var bg = fileName.Split('\\').Last();
+                    Interface.ReportMessage("Changed beatmap background '" + bg + "'");
+                    fileName = Interface.BackgroundImage;
+                }
+            }
+
+            return CreateFileW(
+                fileName,
+                desiredAccess,
+                shareMode,
+                securityAttributes,
+                creationDisposition,
+                flagsAndAttributes,
+                templateFile
+            );
+        }
+
+        #endregion -- Hooks --
+
+        #region -- DLL Methods --
+
+        public EntryPoint(RemoteHooking.IContext context, string channelName)
+        {
+            // Connect to the server object using provided channel name
+            Interface = RemoteHooking.IpcConnectClient<ServerInterface>(channelName);
+
+
+            Interface.Ping();
+        }
+
+        public void Run(RemoteHooking.IContext contect, string channelName)
+        {
+            Interface.IsInstalled(RemoteHooking.GetCurrentProcessId());
+
+            // Install hooks
+
+            // CreateFile https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858(v=vs.85).aspx
+            CreateFileHook = LocalHook.Create(
+                LocalHook.GetProcAddress("kernel32.dll", "CreateFileW"),
+                new CreateFile_Delegate(CreateFile_Hook),
+                this
+            );
+
+
+            // Activate hooks on all threads except the current thread.
+            CreateFileHook.ThreadACL.SetExclusiveACL(new int[] { 0 });
+
+
+            Interface.ReportMessage("Hook 'CreateFile' has been installed");
+
+            RemoteHooking.WakeUpProcess();
+
+            try
+            {
+                // Loop until the loader closes (i.e. IPC fails)
+                while (true)
+                {
+                    Thread.Sleep(500);
+
+                    string[] queued = null;
+
+                    lock (_messageQueue)
+                    {
+                        queued = _messageQueue.ToArray();
+                        _messageQueue.Clear();
+                    }
+
+                    if (queued != null && queued.Length > 0)
+                    {
+                        Interface.ReportMessages(queued);
+                    }
+                    else
+                    {
+                        Interface.Ping();
+                    }
+                }
+            }
+            catch
+            {
+                // Ping() or ReportMessages() will raise an exception if host is unreachable.
+            }
+
+            // Remove hooks
+            CreateFileHook.Dispose();
+
+
+            // Finalize cleanup of hooks
+            LocalHook.Release();
+        }
+
+        #endregion -- DLL Methods --
+
+        #region CreateFileW
+
+        [DllImport("kernel32.dll",
+            CharSet = CharSet.Unicode,
+            SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        static extern IntPtr CreateFileW(
+            String filename,
+            UInt32 desiredAccess,
+            UInt32 shareMode,
+            IntPtr securityAttributes,
+            UInt32 creationDisposition,
+            UInt32 flagsAndAttributes,
+            IntPtr templateFile
+        );
+
+        #endregion CreateFileW Hook
+    }
+}
---
+++ OHook/ServerInterface.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace OHook
+{
+    /**
+     * \class   ServerInterface
+     *
+     * \brief   Provides an interface for communicating from the client (target) to the server (injector)
+     *
+     * \author  Shynd
+     * \date    21.07.2018
+     */
+
+    public class ServerInterface : MarshalByRefObject
+    {
+        private string _backgroundImage;
+        public string BackgroundImage
+        {
+            get { return _backgroundImage; }
+            set { _backgroundImage = value; }
+        }
+
+        public void IsInstalled(int clientPid)
+        {
+            Console.WriteLine("OHook has been injected into process: {0}.\r\n", clientPid);
+        }
+
+        /**
+         * \fn  public void ReportMessages(string[] messages)
+         *
+         * \brief   Output the message to the console.
+         *
+         * \author  Shynd
+         * \date    21.07.2018
+         *
+         * \param   messages    The messages.
+         */
+        public void ReportMessages(string[] messages)
+        {
+            for (var i = 0; i < messages.Length; i++)
+            {
+                Console.WriteLine(messages[i]);
+            }
+        }
+
+        public void ReportMessage(string message)
+        {
+            var time = DateTime.Now.ToString();
+            Console.WriteLine("[*] [" + time + "]: " + message);
+        }
+
+        /**
+         * \fn  public void ReportException(Exception e)
+         *
+         * \brief   Reports an exception
+         *
+         * \author  Shynd
+         * \date    21.07.2018
+         *
+         * \param   e   An Exception to process.
+         */
+        public void ReportException(Exception e)
+        {
+            Console.WriteLine("The target process has reported an error:\r\n" + e);
+        }
+
+        private int _count = 0;
+
+        /**
+         * \fn  public void Ping()
+         *
+         * \brief   Called to confirm that the IPC channel is still open / host application has not closed.
+         *
+         * \author  Shynd
+         * \date    21.07.2018
+         */
+
+        public void Ping()
+        {
+            var oldTop = Console.CursorTop;
+            var oldLeft = Console.CursorLeft;
+            Console.CursorVisible = false;
+
+            var chars = "\\|/-";
+            Console.SetCursorPosition(Console.WindowWidth - 1, oldTop - 1);
+            var loader = chars[_count++ % chars.Length];
+            Console.Write(loader);
+            Console.Title = "OLoader - Connected " + loader;
+
+            Console.SetCursorPosition(oldLeft, oldTop);
+            Console.CursorVisible = true;
+        }
+    }
+}
